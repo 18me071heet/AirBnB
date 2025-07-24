5fbe74b8bde11ba4566ad5a3f65ec3e6c41a30d2
Heet <18ME071.heet.nanda@vvpedulink.ac.in>
2025-07-24 18:03:16 +0530

    Edit Profile page object and Tc script

---
+++ src/test/java/TC_AB/Tc_EditProfile.java
@@ -0,0 +1,59 @@
+package TC_AB;
+
+import org.testng.annotations.Test;
+
+import baseClass.BaseClassAB;
+import pageObject.EditProfile;
+import pageObject.LogInAB;
+
+             public class Tc_EditProfile extends BaseClassAB {
+ 
+	           void threadTime() throws InterruptedException {
+	        	   
+	        	   Thread.sleep(3000);
+	           }
+	   
+	            @Test(priority=1,groups= {"Smoke","Functional"})
+	            void editProfile() throws InterruptedException {
+	            
+	            LogInAB login = new LogInAB(driver);
+		
+				logger.info("Tc-01 --> Verify user is able to enter email ");
+				
+				threadTime();
+				login.emailField(p.getProperty("email"));
+				
+				
+				logger.info("Tc-02 --> Verify user is able to enter password ");
+				threadTime();
+				login.passField(p.getProperty("password"));
+				
+				logger.info("Tc-03 --> Verify user is able to login by clicking login button");
+				Thread.sleep(10000);
+				login.loginClick();
+				
+				EditProfile profile = new EditProfile(driver);
+				
+				threadTime();
+				logger.info("Tc-04 --> Verify side menu is getting open or not by clicking on profile icon");
+				profile.clickProfileIcon();
+				
+				threadTime();
+				logger.info("Tc-05 --> Verify profile page is open or not by click on Profile button");
+				profile.clickProfile();
+				
+				threadTime();
+				logger.info("Tc-06 --> Verify user is able to enter first name only by clickig on Edit ");
+			    profile.clickEdit();
+				
+			    threadTime();
+				profile.enterFname("Johnson");
+		        
+				threadTime();
+				logger.info("Tc-07 --> Verify edited first name is getting saved or not by clicking Save ");
+				profile.saveFname();
+				
+		
+	            }
+				
+}
---
+++ src/test/java/pageObject/EditProfile.java
@@ -0,0 +1,59 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EditProfile {
+
+	
+public WebDriver driver;
+	
+	public EditProfile(WebDriver driver) {
+		
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//img[@src='/assets/header/User.svg']")
+	WebElement profileIcon;
+	
+	@FindBy(xpath="//a[@href='/user/profile' and text()='My Profile']")
+	WebElement profile;
+	
+	@FindBy(xpath="//button[contains(text(),'Edit')])[2]")
+	WebElement editBtn;
+	
+	@FindBy(xpath="//input[@placeholder='Your Name']")
+	WebElement fname;
+	
+	@FindBy(xpath="//button[normalize-space()='Save']")
+	WebElement save;
+	
+	public void clickProfileIcon() {
+		
+		profileIcon.click();
+	}
+	
+	public void clickProfile() {
+		
+		profile.click();
+	}
+	
+	public void clickEdit() {
+		
+		editBtn.click();
+	}
+	
+	public void enterFname(String name) {
+		
+	 fname.sendKeys(name);
+	
+	}
+	
+	public void saveFname() {
+		
+		save.click();
+	}
+}
